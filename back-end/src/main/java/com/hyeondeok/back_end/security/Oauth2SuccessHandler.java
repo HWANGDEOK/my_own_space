3576dc12145d2e5d6fb1148194a507d294fa5c5f
HWANGDEOK <dukjun2002@naver.com>
2026-07-15 08:39:11 +0900

    fix.토큰 유저정보 수정

--- back-end/src/main/java/com/hyeondeok/back_end/security/Oauth2SuccessHandler.java
+++ back-end/src/main/java/com/hyeondeok/back_end/security/Oauth2SuccessHandler.java
@@ -10,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -30,8 +31,9 @@ public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         // Oauth2UserService 에서 생성해서 넘겨준 인증 유저 객체 추출
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
-        // 구글이 준 고유 식별자(sub) 추출
-        String provider = (String) oAuth2User.getAttributes().get("provider");
+        // provider, provider 의 식별자(sub) 추출
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        String provider = oauthToken.getAuthorizedClientRegistrationId();
         String providerId = (String) oAuth2User.getAttributes().get("sub");
 
         // 토큰을 생성하기 위해 해당 유저 조회
