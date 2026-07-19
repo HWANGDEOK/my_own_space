31b8a2286c2dc34f204ac2decbb9f26a1a0e7a7d
HWANGDEOK <dukjun2002@naver.com>
2026-07-20 06:34:33 +0900

    fix.AdminProperties Component 설정

--- back-end/src/main/java/com/hyeondeok/back_end/config/AdminProperties.java
+++ back-end/src/main/java/com/hyeondeok/back_end/config/AdminProperties.java
@@ -3,9 +3,11 @@ package com.hyeondeok.back_end.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "admin")
 public class AdminProperties {
     private String activationKey;
